d9ffa3d179307d99023957b36e4e72948e9b9577
Merp <merrillamyersiii@gmail.com>
2013-02-26 01:37:56 -0500

    Add simplecombobox class for quick combobox selections.

---
+++ SharpTune/GUI/SimpleCombo.cs
@@ -0,0 +1,42 @@
+﻿/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SharpTune
+{
+    public static class SimpleCombo
+    {
+        public static string ShowDialog(string text, string caption, List<string> comboitems)
+        {
+            Form prompt = new Form();
+            prompt.Width = 500;
+            prompt.Height = 200;
+            prompt.Text = caption;
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 300 };
+            ComboBox comboBox = new ComboBox() { Left = 50, Top = 50, DataSource = comboitems, Width = 400 };
+            Button confirmation = new Button() { Text = "OK", Left = 350, Width = 100, Top = 70 };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(comboBox);
+            prompt.AcceptButton = confirmation;
+            prompt.ShowDialog();
+            return comboBox.SelectedItem.ToString();
+        }
+    }
+
+}
